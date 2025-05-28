0b41f428654249f8832e2e0012258ad3bf873a19
ziyamoosir <ziyamoosir@gmail.com>
2025-05-28 13:32:01 +0530

    Fix blank lines to satisfy flake8

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from flask import Flask
 
+
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello from VedAlert backend!"
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route("/")
+def hello():
+    return "Hello, VedAlert!"
 
