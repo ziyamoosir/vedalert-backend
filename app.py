a993730f8509e6145e486659b3e5182ac34cb718
ziyamoosir <ziyamoosir@gmail.com>
2025-05-28 13:29:29 +0530

    Add basic Flask app with home route

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-"# Backend entry point"
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from VedAlert backend!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
